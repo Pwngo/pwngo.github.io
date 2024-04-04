ab07f6ef5503c22cd6252f82aa81d2bf79dcc928
Pwngo <161769169+Pwngo@users.noreply.github.com>
2024-04-04 15:18:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// script.js
-
-// When the DOM is fully loaded
-
 document.addEventListener('DOMContentLoaded', function() {
   var navLinks = document.querySelectorAll('nav ul li a');
 
   navLinks.forEach(function(link) {
    link.addEventListener('click', function(event) {
       event.preventDefault();
+     window.location.reload();
       var targetId = this.getAttribute('href').slice(1);
       var targetSection = document.getElementById(targetId);
       targetSection.scrollIntoView({ behavior: 'smooth' });
     });
   });
 });
-
