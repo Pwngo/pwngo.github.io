6874d98d8c4d995ba05f346bc68f70560a0a00a9
Pwngo <161769169+Pwngo@users.noreply.github.com>
2024-04-04 15:41:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+var x;
+function show(elem){
+  for(var i = 0; i < x.length i++){
+    x[i].style.display = "none";
+  }
+  elem.style.display = "block";
+}
 document.addEventListener('DOMContentLoaded', function() {
   var navLinks = document.querySelectorAll('nav ul li a');
-
+x = document.querySelectorAll("section");
   navLinks.forEach(function(link) {
    link.addEventListener('click', function(event) {
       event.preventDefault();
      window.location.reload();
       var targetId = this.getAttribute('href').slice(1);
       var targetSection = document.getElementById(targetId);
-      targetSection.scrollIntoView({ behavior: 'smooth' });
+      show(targetSection);
     });
   });
 });
