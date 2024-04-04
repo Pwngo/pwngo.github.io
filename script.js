a738745fa1b87fda0b1f289e0a82abba7002b6af
LJMSTSAstudent <162632476+LJMSTSAstudent@users.noreply.github.com>
2024-04-04 15:17:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 
 // When the DOM is fully loaded
 
-//document.addEventListener('DOMContentLoaded', function() {
-//  var navLinks = document.querySelectorAll('nav ul li a');
+document.addEventListener('DOMContentLoaded', function() {
+  var navLinks = document.querySelectorAll('nav ul li a');
 
-//  navLinks.forEach(function(link) {
-//    link.addEventListener('click', function(event) {
-//      event.preventDefault();
-//      var targetId = this.getAttribute('href').slice(1);
-//      var targetSection = document.getElementById(targetId);
-//      targetSection.scrollIntoView({ behavior: 'smooth' });
-//    });
-//  });
-//});
+  navLinks.forEach(function(link) {
+   link.addEventListener('click', function(event) {
+      event.preventDefault();
+      var targetId = this.getAttribute('href').slice(1);
+      var targetSection = document.getElementById(targetId);
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+});
 
