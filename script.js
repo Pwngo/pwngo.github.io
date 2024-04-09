56195670a6edab651f2e8b53c0f2dd10052cf1e8
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-04-09 16:49:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function show(x, elem){
+  try{
   for(var i = 0; i < x.length i++){
     x[i].style.display = "none";
   }
   elem.style.display = "block";
+  } catch(e){
+    alert(e.message);
+  }
 }
+try{
 show(document.querySelectorAll("section"), document.getElementById("events"));
+} catch(e){
+  alert(e.message);
+}
