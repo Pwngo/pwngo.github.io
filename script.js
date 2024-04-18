70fabc6b96f935a32905b646d970025f0ac400d1
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-04-18 16:17:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var currenturl = location.href.replace(location.origin + "/", "");
 if(currenturl == ""){
   currenturl = "index.html";
 }
+alert(currenturl);
 var links = document.querySelectorAll("a");
 for(var i = 0, i < links.length; i++){
 if(links[i].href == currenturl){
