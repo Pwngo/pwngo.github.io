08bf03bf7ebccc1d010c18c56f07125601f2a998
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-04-25 21:11:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ if(currenturl == ""){
 var links = document.querySelectorAll("a");
 for(var i = 0; i < links.length; i++){
 if(links[i].href == currenturl){
+alert(links[i]);
 links[i].style.fontWeight = "bold";
 }
 }
