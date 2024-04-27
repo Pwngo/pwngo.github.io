408dcf51255c2bb7841ef0e0079605ff7215cc8e
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-04-27 14:33:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ if (!link) {
 }
 link.href = 'https://stackoverflow.com/favicon.ico';
 }
+changeFavicon("/SDG_logo.png")
 var currenturl = location.href.replace(location.origin + "/", "");
 if(currenturl == ""){
   currenturl = "index.html";
